9edb0d7d9081aa0d851eb157902b8ff1beba1166
Oxamyt <dentez.danil@gmail.com>
2023-12-27 17:02:08 +0200

    fix-status

--- script.js
+++ script.js
@@ -1,28 +1,12 @@
-// My problem is to make responsible tic tac toe game, where two players mark empty  cells with O and X and then when 3 across is same(like O-O-O or 0) then player wins.
-// 0
-// 0
-// Plan
-// My game must include user interface where players can click on empty cell and fill it with their mark.
-// I will get user input from clicks.
-// Desired output is to be able to fill empty cells and check weather a player has won.
-// 1. When the user clicks on empty cell
-// 2. Check if he`s playerOne or playerTwo
-// 3. Fill in mark of playerOne or playerTwo.
-// 4. Check if player won and if not give turn to other player and repeat from step 2.
-// Start from making boardFunction
-// Make game control
-// Make player input feature.
-
 // Create the game boardFunction
 const boardFunction = (function () {
-  const board = [
+  let board = [
     ["", "", ""],
     ["", "", ""],
     ["", "", ""],
   ];
   return board;
 })();
-
 
 function createPlayer(name, marker) {
   return {
@@ -33,10 +17,11 @@ function createPlayer(name, marker) {
 
 const playerOne = createPlayer("PlayerOne", "O");
 const playerTwo = createPlayer("PlayerTwo", "X");
-
+const status = document.querySelector(".status");
 const players = [playerOne, playerTwo];
 
 let activePlayer = players[0].marker;
+let isEnd = false;
 
 const switchPlayerTurn = function () {
   activePlayer =
@@ -45,6 +30,7 @@ const switchPlayerTurn = function () {
 };
 // Place the player's mark
 function placeMark(row, col) {
+  if (isEnd) return;
   if (boardFunction[row][col].length === 0) {
     boardFunction[row][col] = mark = switchPlayerTurn();
     render();
@@ -54,33 +40,36 @@ function placeMark(row, col) {
 
 // Check for a winner
 function checkWinner() {
-  const status = document.querySelector('.status');
   for (let i = 0; i < 3; i++) {
     if (
       boardFunction[i][0] == "X" &&
       boardFunction[i][1] == boardFunction[i][0] &&
       boardFunction[i][1] == boardFunction[i][2]
     ) {
-      status.innerText = 'Player Two Wins!';
+      status.innerText = "Player Two Wins!";
+      isEnd = true;
     } else if (
       boardFunction[i][0] == "O" &&
       boardFunction[i][1] == boardFunction[i][0] &&
       boardFunction[i][1] == boardFunction[i][2]
     ) {
-      status.innerText = 'Player One Wins!';
+      status.innerText = "Player One Wins!";
+      isEnd = true;
     }
     if (
       boardFunction[0][i] == "X" &&
       boardFunction[1][i] == boardFunction[0][i] &&
       boardFunction[1][i] == boardFunction[2][i]
     ) {
-      status.innerText = 'Player Two Wins!';
+      status.innerText = "Player Two Wins!";
+      isEnd = true;
     } else if (
       boardFunction[0][i] == "O" &&
       boardFunction[1][i] == boardFunction[0][i] &&
       boardFunction[1][i] == boardFunction[2][i]
     ) {
-      status.innerText = 'Player One Wins!';
+      status.innerText = "Player One Wins!";
+      isEnd = true;
     }
   }
   if (
@@ -88,28 +77,32 @@ function checkWinner() {
     boardFunction[1][1] == boardFunction[0][0] &&
     boardFunction[2][2] == boardFunction[1][1]
   ) {
-    status.innerText = 'Player Two Wins!';
+    status.innerText = "Player Two Wins!";
+    isEnd = true;
   }
   if (
     boardFunction[0][2] == "X" &&
     boardFunction[1][1] == boardFunction[0][2] &&
     boardFunction[2][0] == boardFunction[1][1]
   ) {
-    status.innerText = 'Player Two Wins!';
+    status.innerText = "Player Two Wins!";
+    isEnd = true;
   }
   if (
     boardFunction[0][0] == "O" &&
     boardFunction[1][1] == boardFunction[0][0] &&
     boardFunction[2][2] == boardFunction[1][1]
   ) {
-    status.innerText = 'Player One Wins!';
+    status.innerText = "Player One Wins!";
+    isEnd = true;
   }
   if (
     boardFunction[0][2] == "O" &&
     boardFunction[1][1] == boardFunction[0][2] &&
     boardFunction[2][0] == boardFunction[1][1]
   ) {
-    status.innerText = 'Player One Wins!';
+    status.innerText = "Player One Wins!";
+    isEnd = true;
   }
   if (
     boardFunction[0][0] !== "" &&
@@ -122,12 +115,23 @@ function checkWinner() {
     boardFunction[2][1] !== "" &&
     boardFunction[2][2] !== ""
   ) {
-    status.innerText = 'Draw!';
+    status.innerText = "Draw!";
+    isEnd = true;
   }
 }
+
+const gameboard = document.querySelector(".gameboard");
+
+gameboard.addEventListener("click", (e) => {
+  if (e.target == e.currentTarget) return;
+  const i = e.target.getAttribute("data-i");
+  const j = e.target.getAttribute("data-j");
+  placeMark(i, j);
+});
+
 function render() {
-  startBtn.style.display = 'none';
-  const gameboard = document.querySelector(".gameboard");
+  if (isEnd) return;
+  startBtn.style.display = "none";
   gameboard.innerHTML = "";
   for (let i = 0; i < boardFunction.length; i++) {
     for (let j = 0; j < boardFunction.length; j++) {
@@ -139,16 +143,23 @@ function render() {
       cell.innerText = cellMark;
       gameboard.appendChild(cell);
     }
-    gameboard.addEventListener("click", (e) => {
-      if (e.target == e.currentTarget) return;
-      const i = e.target.getAttribute("data-i");
-      const j = e.target.getAttribute("data-j");
-      placeMark(i, j);
-    });
   }
 }
 
-const cell = document.querySelector(".cell");
-const startBtn = document.querySelector('.start')
-startBtn.addEventListener('click', render);
+function restart() {
+  isEnd = false;
+  status.innerText = "";
+  for (let i = 0; i < boardFunction.length; i++) {
+    for (let j = 0; j < boardFunction.length; j++) {
+      boardFunction[i][j] = "";
+    }
+  }
+  activePlayer = players[0].marker;
+  render();
+}
 
+const cell = document.querySelector(".cell");
+const startBtn = document.querySelector(".start");
+startBtn.addEventListener("click", render);
+const resBtn = document.querySelector(".restartbtn");
+resBtn.addEventListener("click", restart);
